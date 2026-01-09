ea5bf20b352792139b298bbab710df607859e8f2
henrique.matheus <henriquematheussilva.21@gmail.com>
2026-01-08 22:51:39 -0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+//Declarando variáveis
+let nome = "henrique.matheus";
+let xp = 7000;
+let nivel = "";
+
+
+//Estrutura de decisão conforme nível de XP
+if (xp < 1000) {
+  nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+  nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+  nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+  nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+  nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+  nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+
+//Saída dependendo do nível de XP
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
